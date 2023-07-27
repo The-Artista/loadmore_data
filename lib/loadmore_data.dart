53beb0d46f981e66de15ff507e8c033976a4f324
ishaf <shafiulislam20@gmail.com>
2023-07-27 17:08:27 +0600

    build(test): test ci

--- lib/loadmore_data.dart
+++ lib/loadmore_data.dart
@@ -1,4 +1,4 @@
-/// loadmore_data is a flutter package that can handel infinity scroll pagination
+/// load-more_data is a flutter package that can handel infinity scroll pagination
 library loadmore_data;
 
-export 'src/loadmore_data.dart';
+export 'src/load-more_data.dart';
